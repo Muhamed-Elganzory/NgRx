2c0af797b7df6bba1b4c302eb08bba512d358a89
Muhamed-Elganzory <mohamedelganzory621@gmail.com>
2025-03-25 21:39:33 +0200

    end

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,5 +14,5 @@ export const appConfig: ApplicationConfig = {
         counter: counterReducer
     }),
     provideEffects([CounterEffects])
-]
+  ]
 };
--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
-import {AsyncPipe} from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import {counterSelector, counterDoubleSelector} from '../Store/counter.selector';
 
 @Component({
